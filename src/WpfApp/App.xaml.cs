a1702d42638d7f2a605d9dabe940207b68d16b8b
nazarovqurbonali <nazarovqurbonali4@gmail.com>
2025-05-30 15:32:40 +0500

    feature: add file global-using

--- src/WpfApp/App.xaml.cs
+++ src/WpfApp/App.xaml.cs
@@ -1,13 +1,9 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
-
-namespace WpfApp;
+﻿namespace WpfApp;
 
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application
+public partial class App 
 {
 }
 
